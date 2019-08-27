10c4e1bb536efed1db375da535031932bcdb57b9
Hall Liu <hallliu@google.com>
2019-08-27 16:52:40 -0700

    Add carrier ID to telecom metrics proto
    Add the carrier ID so it can be used in server-side aggregation
    
    Bug: 119153703
    Test: unit, manual
    Change-Id: I10212411741850169ecb7712f60b2275620b3943
    (cherry picked from commit e3fc38d188f6061427e8d2341370366838ba21ff)

--- tests/src/com/android/server/telecom/tests/ComponentContextFixture.java
+++ tests/src/com/android/server/telecom/tests/ComponentContextFixture.java
@@ -209,6 +209,10 @@ public class ComponentContextFixture implements TestFixture<Context> {
                 return Context.AUDIO_SERVICE;
             } else if (svcClass == TelephonyManager.class) {
                 return Context.TELEPHONY_SERVICE;
+            } else if (svcClass == CarrierConfigManager.class) {
+                return Context.CARRIER_CONFIG_SERVICE;
+            } else if (svcClass == SubscriptionManager.class) {
+                return Context.TELEPHONY_SUBSCRIPTION_SERVICE;
             }
             throw new UnsupportedOperationException();
         }
